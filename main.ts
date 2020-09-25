210ca39e20646db02e30a584f27658aaa5fb7113
keithlai555 <71888224+keithlai555@users.noreply.github.com>
2020-09-25 13:53:22 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,9 +4,9 @@ input.onGesture(Gesture.TiltLeft, function () {
 input.buttonA.onEvent(ButtonEvent.Click, function () {
     makerController.player1.press(ArcadeButton.A)
     music.pewPew.play()
-    light.setPixelColor(0, 0xff8000)
+    light.setPixelColor(0, 0x0000ff)
     pause(100)
-    light.setPixelColor(0, 0x000000)
+    light.setPixelColor(0, 0x999999)
 })
 input.onGesture(Gesture.TiltRight, function () {
     makerController.player1.press(ArcadeButton.Right)
@@ -14,17 +14,16 @@ input.onGesture(Gesture.TiltRight, function () {
 input.buttonB.onEvent(ButtonEvent.Click, function () {
     makerController.player1.press(ArcadeButton.B)
     music.jumpUp.play()
-    light.setPixelColor(9, 0xff0080)
+    light.setPixelColor(9, 0xff0000)
     pause(100)
-    light.setPixelColor(9, 0x000000)
+    light.setPixelColor(9, 0x999999)
 })
 music.powerUp.play()
-light.setAll(0xffff00)
-for (let index = 0; index <= 85; index++) {
+light.setAll(0x999999)
+for (let index = 0; index <= 65; index++) {
     light.setBrightness(index)
 }
-light.setAll(0x7f00ff)
-light.setBrightness(85)
+light.setBrightness(5)
 forever(function () {
     makerController.player1.setAnalog(ArcadeAnalogButton.LeftRight, input.acceleration(Dimension.X))
 })
